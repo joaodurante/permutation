27bba67ad411ca84076467c6a5001185db513dc8
João Pedro De Munno Durante <jaodurante@gmail.com>
2018-11-14 10:56:04 -0200

    Code improvements

--- permutation.cpp
+++ permutation.cpp
@@ -9,19 +9,19 @@ void swap(char string[], int i, int j);
 
 int main(){
 	char string[STRING_SIZE] = STRING;
-	int stringSize = STRING_SIZE - 2; //STRING_SIZE - "\0" - last char
+	int stringSize = STRING_SIZE - 1; //STRING_SIZE - "\0"
 	permute(string, 0, stringSize);
 	getchar();
 }
 
 void permute(char string[], int start, int end){
 	if(start == end){
-		for(int i = 0; i <= end; i++)
+		for(int i = 0; i < end; i++)
 			printf("%c ", string[i]);
 		printf("\n");
 	}
 	else{
-		for(int i = start; i <= end; i++){
+		for(int i = start; i < end; i++){
 			swap(string, start, i);
 			permute(string, start + 1, end);
 			swap(string, start, i);
